ca987c0f6fa2f5000d880e35bc3f37047582e7e2
Dan Singer <d_singer@fanshaweonline.ca>
2024-01-02 17:43:16 -0500

    Index Content
    Finished the content for the index

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 (() => {
 
+    const hamMenu = document.querySelector("#ham-icon")
+    const linkMenu = document.querySelector("#links")
 
+    function openMenu() {
+        if (linkMenu.style.display === "flex") {
+            linkMenu.style.display = "none"
+        } else {
+            linkMenu.style.display = "flex"
+        }
+    }
+
+    hamMenu.addEventListener("click", openMenu)
 
 })();
